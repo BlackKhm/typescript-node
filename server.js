ce3cca3a48d0ce27e3f813cf21605c119f1ceb6d
mao.thach@z1fsc.com <48387259+BlackKhm@users.noreply.github.com>
2022-09-04 18:13:11 +0700

    install nodemon

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/routeName", (req, res) => {
+  res.send("Hello routeName!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
