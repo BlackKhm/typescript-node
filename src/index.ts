0c17d960f067c01c5b36b1400063ee162132ea67
mao.thach@z1fsc.com <48387259+BlackKhm@users.noreply.github.com>
2022-09-04 18:43:41 +0700

    first test create project

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+const epxress = require("express");
+const app = epxress();
+const hostName = "127.0.0.1";
+const port = 3000;
+
+app.set('view engine', 'ejs');
+// app.use(express.static('public'));
+
+app.get('/', function (req : any , res : any) {
+  res.render("index",{name: ""});
+})
+
+app.listen(port,hostName, function(){
+    console.log(`Serve runnign at http://${hostName}:${port}`);
+});
