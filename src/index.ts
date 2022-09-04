7af6a115c6d3b43ab29d9403b39ce59aa5771b0b
mao.thach@z1fsc.com <48387259+BlackKhm@users.noreply.github.com>
2022-09-04 18:58:49 +0700

    install grapql

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import { rootQuery, schema } from "./schema/schema";
 const epxress = require("express");
+import { graphqlHTTP } from 'express-graphql';
+
 const app = epxress();
 const hostName = "127.0.0.1";
 const port = 3000;
@@ -9,6 +12,12 @@ app.set('view engine', 'ejs');
 app.get('/', function (req : any , res : any) {
   res.render("index",{name: ""});
 })
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: rootQuery,
+  graphiql: true,
+}));
 
 app.listen(port,hostName, function(){
     console.log(`Serve runnign at http://${hostName}:${port}`);
---
+++ src/schema/schema.ts
@@ -0,0 +1,20 @@
+
+import {buildSchema} from 'graphql';
+
+export const  schema = buildSchema(`type Query {
+    firs_name: String,
+    last_name: String
+  }
+  `);
+// The root provides a resolver function for each API endpoint
+export const  rootQuery = {
+    firs_name: () => {
+        return 'Hello firsname!';
+    },
+    last_name: () => {
+        return 'Hello last_name!';
+    },
+};
+
+
+
